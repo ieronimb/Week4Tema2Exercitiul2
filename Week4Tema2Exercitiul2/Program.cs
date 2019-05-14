f1d85a338defd10f56c02729a3b63846171c17f5
Birnat Ieronim <Ieronimb@outlook.com>
2019-05-14 16:31:35 +0300

    Add project files.

---
+++ Week4Tema2Exercitiul2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Tema2Exercitiul2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack newStack = new Stack(5);
+            newStack.push("Constantin");
+            newStack.push("Elena");
+            newStack.push("Dumitru");
+            newStack.push("Ieronim");
+            newStack.push("Andreea");
+            Console.WriteLine("\n");
+
+            //Push next row (from end to top)
+            Console.WriteLine(newStack.peek() + " name is at the top o the stack");
+
+            //Pop
+            while (!newStack.isEmpty())
+            {
+                string names = newStack.pop();
+                Console.WriteLine("The name is {0}", names);
+            }
+        }
+    }
+}
---
+++ Week4Tema2Exercitiul2/Stack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Tema2Exercitiul2
+{
+    class Stack
+    //M-am inspirat din https://www.lynda.com/C-tutorials/Stack-explained/604241/636309-4.html
+    {
+        //Variables or fields
+        private int maxSize;
+        private string[] stackArray;
+        private int top;
+
+        //Assign values in Constructor
+        public Stack(int size)
+        {
+            maxSize = size;
+            stackArray = new string[maxSize];
+            top = -1;
+        }
+
+        //Push Method
+        public void push(string m)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("The stack is full");
+            }
+            else
+            {
+                top++;
+                stackArray[top] = m;
+            }
+        }
+        //Pop Method
+        public string pop()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("The stack is empty");
+                return "--";
+            }
+            else
+            {
+                int oldTop = top;
+                top--;
+                return stackArray[oldTop];
+            }
+        }
+        //Peek Method
+        public string peek()
+        {
+            return stackArray[top];
+        }
+
+        public bool isEmpty()
+        {
+            return (top == -1);
+        }
+
+        private bool isFull()
+        {
+            return (maxSize - 1 == top);
+        }
+    }
+}
